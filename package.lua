7a50f601f9c09c1399d38936a70087f05af7c26d
EliezerBrasilian <alpinistamestre@yahoo.com>
2025-04-26 11:34:17 -0300

    bump version to 1.1 and update dependencies in pom.xml

--- package.lua
+++ package.lua
@@ -5,7 +5,7 @@
 Name = "json_mapper"
 DirectoryWhereMainFileIs = "brasilian.json_mapper.App"
 Description = "A powerful JSON mapping and manipulation library for Lua. Easily convert between JSON structures and Lua objects with type validation and data transformation support. Perfect for APIs, data processing, and web service integration."
-Version = "0.0.1"
+Version = "1.1"
 
 RepositoryUrl = ""
 
